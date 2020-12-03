78b1e3b54f07bc112cd71ec91c6e0738a4a91a5f
Omer <omercinal@gmail.com>
2020-12-03 14:31:09 +0300

    added manual server management

--- nfs-ds/client.py
+++ nfs-ds/client.py
@@ -13,7 +13,10 @@ from pick import pick
 class Client:
     SEPARATOR = " := "
     BREAK = " "
+    CANCEL = Symbols.make_option("Cancel")
     REFRESH = Symbols.make_option("Refresh Servers")
+    MANUAL_CONNECT = Symbols.make_option("Add Server Manually")
+    REMOVE_SERVER = Symbols.make_option("Remove Server")
     EXIT = Symbols.make_option("Exit")
 
     def __init__(self):
@@ -21,7 +24,6 @@ class Client:
 
     def _scan_servers(self):
         self._servers["Localhost"] = {"host": "localhost", "port": 50051}
-        self._servers["Test"] = {"host": "192.168.1.37", "port": 50051}
 
     def _connect_to_server(self, server_name: str):
         server_name, _ = server_name.split(self.SEPARATOR)
@@ -37,10 +39,37 @@ class Client:
             server_names.append(f"{name}{self.SEPARATOR}({host}:{port})")
         return sorted(server_names)
 
+    def _add_manual_server(self):
+        server_name = input("Enter server name (alias).\n>")
+        server_info = input("Enter the ip of the server. Example: [0.0.0.0:50051]\n>")
+        host, port = server_info, "50051"
+        if ":" in server_info:
+            host, port = server_info.split(":")
+        
+        self._servers[server_name] = {
+            "host": host,
+            "port": port,
+        }
+
+    def _remove_server(self):
+        while True:
+            options = self._get_server_names() + [self.BREAK, self.CANCEL]
+            option, index = pick(options, "Select a server to remove.")
+
+            if option == self.BREAK:
+                continue
+            elif option == self.CANCEL:
+                break
+            else:
+                server_name, _ = option.split(self.SEPARATOR)
+                del self._servers[server_name]
+                break
+
+
     def start(self):
         self._scan_servers()
         while True:
-            options = self._get_server_names() + [self.BREAK, self.REFRESH, self.EXIT]
+            options = self._get_server_names() + [self.BREAK, self.REFRESH, self.MANUAL_CONNECT, self.REMOVE_SERVER, self.EXIT]
             option, index = pick(options, f"{len(self._servers)} servers found.")
             if option == self.EXIT:
                 break
@@ -48,6 +77,10 @@ class Client:
                 self._scan_servers()
             elif option == self.BREAK:
                 continue
+            elif option == self.MANUAL_CONNECT:
+                self._add_manual_server()
+            elif option == self.REMOVE_SERVER:
+                self._remove_server()
             else:
                 stub, channel = self._connect_to_server(option)
                 PageNavigator(stub).loop_pages()
@@ -56,5 +89,4 @@ class Client:
 
 
 if __name__ == "__main__":
-    client = Client()
-    client.start()
+    Client().start()
